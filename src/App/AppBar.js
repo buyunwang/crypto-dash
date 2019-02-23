7b609d0b539774d5fd76f2f671de2e8d9d3ef64b
buyunwang <buyunwang@hotmail.com>
2019-02-22 23:59:39 -0800

    add styling

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from "styled-components";
 
+const Logo= styled.div`
+`
+
 const Bar = styled.div`
     display:grid;
     grid-template-columns: 180px auto 100px 100px;
