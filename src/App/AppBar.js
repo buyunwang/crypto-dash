3aadfb71ee8778b49355797dba8c1eb845c04b57
buyunwang <buyunwang@hotmail.com>
2019-02-24 19:47:42 -0800

    add react context

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled, {css} from "styled-components";
+import {AppContext} from './AppProvider';
 
 const Logo= styled.div`
   font-size:1.5em;
@@ -22,11 +23,17 @@ function toProperCase(lower){
   return lower.charAt(0).toUpperCase()+lower.substr(1);
 }
 
-function ControlButton({name, active}){
+function ControlButton({name}){
   return (
-    <ControlButtonElem active={active}>
-    {toProperCase(name)}
-    </ControlButtonElem>
+    <AppContext.Consumer>
+    {({page, setPage}) =>(
+        <ControlButtonElem 
+        active={page === name}
+        onClick={()=>setPage(name)}>
+          {toProperCase(name)}
+        </ControlButtonElem>
+    )}
+    </AppContext.Consumer>
   )
 }
 
@@ -35,7 +42,7 @@ export default function(){
       <Bar>
             <Logo>CryptoDash</Logo>
             <div/>
-            <ControlButton active name="dashboard"/>
+            <ControlButton name="dashboard"/>
             <ControlButton name="settings" />
       </Bar>
     );
