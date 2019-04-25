3f028134a88a7076707ff2339ddfbe25c9cf0da9
buyunwang <buyunwang@hotmail.com>
2019-04-24 22:12:04 -0700

    add loading content

--- src/App/index.js
+++ src/App/index.js
@@ -5,14 +5,16 @@ import styled,{css} from 'styled-components';
 import AppLayout from './AppLayout'
 import AppBar from './AppBar';
 import {AppProvider} from './AppProvider';
-
+import Content from '../Shared/Content';
 class App extends Component {
   render() {
     return (
       <AppLayout>
         <AppProvider>
           <AppBar />
-          <Settings />
+          <Content>
+            <Settings />
+          </Content>
         </AppProvider>
       </AppLayout>
     );
