6e9ce6aa9d94d5c8d6fff17777b2a376372261b0
buyunwang <wby920923@gmail.com>
2020-12-05 20:17:03 -0800

    add fuzzy search

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -10,20 +10,25 @@ export const CoinGridStyled = styled.div`
     margin-top: 40px;
 `
 
-function getCoinsToDisplay(coinList, topSection, favorites){
-    return topSection? favorites : Object.keys(coinList).slice(0, topSection? 10:100);
+function getLowerSectionCoins(coinList, filteredCoins){
+    return (filteredCoins && Object.keys(filteredCoins)) ||
+        Object.keys(coinList).slice(0, 100)
 }
 
-export default function({topSection}){
+function getCoinsToDisplay(coinList, topSection, favorites, filterCoins){
+    return topSection ? favorites : getLowerSectionCoins(coinList, filterCoins);
+}
+
+export default function ({topSection}){
     return (
-      <AppContext.Consumer>
-        {({ coinList, favorites }) => (
-          <CoinGridStyled>
-            {getCoinsToDisplay(coinList, topSection, favorites).map(coinKey => (
-              <CoinTile topSection={topSection} coinKey={coinKey}/>
-            ))}
-          </CoinGridStyled>
-        )}
-      </AppContext.Consumer>
+        <AppContext.Consumer>
+            {({coinList, favorites, filteredCoins}) => (
+                <CoinGridStyled>
+                    {getCoinsToDisplay(coinList, topSection, favorites, filteredCoins).map(coinKey =>
+                        <CoinTile key={coinKey} topSection={topSection} coinKey={coinKey}/>
+                    )}
+                </CoinGridStyled>
+            )}
+        </AppContext.Consumer>
     );
 }
