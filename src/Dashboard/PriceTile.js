5ec02e06a937cf724b7827bae3f565bb2eb5937c
buyunwang <wby920923@gmail.com>
2021-03-31 23:58:09 -0700

    Update PriceTile.js

--- src/Dashboard/PriceTile.js
+++ src/Dashboard/PriceTile.js
@@ -2,8 +2,14 @@ import React from 'react'
 import styled, {css} from 'styled-components'
 import {SelectableTile} from "../Shared/Tile";
 
+const PriceTileStyled = styled(SelectableTile)
+
 export default ({price, index}) => {
     let sym = Object.keys(price)[0]
     let data = price[sym]['USD']
-    return <SelectableTile>{sym} {data.PRICE}</SelectableTile>
+    return (
+        <SelectableTile>
+            {sym}
+            {data.PRICE}
+        </SelectableTile>)
 }
