424a56bf1f924f51261c6614c17e4dc59ca8d536
buyunwang <wby920923@gmail.com>
2021-04-02 20:00:36 -0700

    Update PriceTile.js

--- src/Dashboard/PriceTile.js
+++ src/Dashboard/PriceTile.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled, {css} from 'styled-components'
 import {SelectableTile} from "../Shared/Tile"
 import {fontSize3} from "../Shared/Styles"
+import {CoinHeaderGridStyled} from "../Settings/CoinHeaderGrid"
 
 const PriceTileStyled = styled(SelectableTile)`
     ${props => props.compact && css`
@@ -9,12 +10,29 @@ const PriceTileStyled = styled(SelectableTile)`
     `}
 `
 
+const numberFormat = number => {
+    return +(number + '').slice(0, 7)
+}
+
+const PriceTile = ({sym, data}) => {
+    return (
+        <PriceTileStyled>
+            <CoinHeaderGridStyled>
+                <div>
+                    {sym}
+                </div>
+                <div>{numberFormat(data.CHANGEPCT24HOUR)}</div>
+            </CoinHeaderGridStyled>
+        </PriceTileStyled>
+    )
+}
+
 export default ({price, index}) => {
     let sym = Object.keys(price)[0]
     let data = price[sym]['USD']
     return (
-        <PriceTileStyled compact={index >= 5}>
+        <PriceTile sym={sym} data={data}>
             {sym}
             {data.PRICE}
-        </PriceTileStyled>)
+        </PriceTile>)
 }
