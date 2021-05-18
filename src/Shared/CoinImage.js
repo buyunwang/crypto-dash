fb7e10a77efbdd2bb6176b805df92384ed8b384c
buyunwang <wby920923@gmail.com>
2021-05-17 21:39:19 -0700

    fix style

--- src/Shared/CoinImage.js
+++ src/Shared/CoinImage.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled, {css} from 'styled-components'
 
 const CoinImage = styled.img`
-    height:'50px';
+    height: 50px;
     ${props => props.spotlight && css`
         height: 200px;
         margin: auto;
@@ -15,5 +15,5 @@ export default function({coin, spotlight}){
         spotlight={spotlight}
         alt={coin.CoinSymbol}
         src={`http://cryptocompare.com/${coin.ImageUrl}`}
-    />;
+    />
 }
