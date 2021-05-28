1854be2907f71bb9c4038783096b0d609ec19de3
buyunwang <buyunwang@hotmail.com>
2021-05-27 22:23:32 -0700

    add light theme

--- src/Dashboard/HighchartsTheme.js
+++ src/Dashboard/HighchartsTheme.js
@@ -1,12 +1,14 @@
+import { lightTheme } from '../Shared/Styles'
+
 export default {
     colors: ['#61d936', '#552ccb', '#1163c9', '#04a1ee', '#08c6e0',
         '#146b9e', '#f376c1', '#1b2839'],
     chart: {
-        backgroundColor: '#061a44',
+        backgroundColor: lightTheme ? 'white' : '#061a44',
         borderColor: '#000000',
         borderWidth: 0,
         className: 'dark-container',
-        plotBackgroundColor: '#061a44',
+        plotBackgroundColor: lightTheme ? 'white' : '#061a44',
         plotBorderColor: '#CCCCCC',
         plotBorderWidth: 0
     },
